d91cb3fd99c09f932d4730de919c4bb3660e68cd
konhi <hello.konhi@gmail.com>
2021-09-26 15:39:33 +0200

    feat: dynamic desc

--- README.md
+++
@@ -1,1 +0,0 @@
-<img width="35%" src="https://github-readme-stats.vercel.app/api?username=konhi&hide=stars&show_icons=true&theme=dark&hide_rank=true&custom_title=Nice to meet you! 😊&line_height=32">
---
+++ index.js
@@ -0,0 +1,61 @@
+import fetch from "node-fetch";
+import { config } from "dotenv";
+import fs from "fs";
+import moment from 'moment';
+
+config();
+moment().format();
+
+const GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"
+
+async function getStats() {
+  const query = `
+  {
+    viewer {
+      contributionsCollection {
+        totalIssueContributions
+        totalPullRequestContributions
+        endedAt
+      }
+    }
+  }
+  `
+
+  const stats = fetch(GITHUB_GRAPHQL_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify({query}),
+    headers: {
+      'Authorization': `Bearer ${process.env.token}`
+    }
+  })
+    .then(response => response.json())
+
+  return stats
+}
+
+const res = await getStats()
+const stats = res.data.viewer.contributionsCollection
+
+
+const text = `
+<center>
+👋 Hello! I'm <b>${moment("20050512").fromNow(true)} old</b> student from <b>Poland!</b>
+
+
+<br>
+<br>
+
+🔮 <b>${stats.totalIssueContributions}</b> Issues ✨ <b>${stats.totalPullRequestContributions}</b> Pull Requests ⌚ Last Commit <b>${moment(stats.endedAt).toNow()}</b>
+
+<br>
+<center>
+
+😊 I'm always looking for cool opportunities! Feel free to contact me throught Discord <b><konhi#1588></b> or <b>hello.konhi@gmail.com</b>
+</center>
+<br>
+
+💛 JavaScript   💚 Node.js   💙 Python   🧡 Web Dev   💖 Open-source   🐧 Linux   🐱‍💻 Git   🎨 UI
+</center>
+`
+
+fs.writeFileSync('readme.md', text)
